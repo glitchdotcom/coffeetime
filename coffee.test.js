6be3ee7fc2b2451d936167982ef43564e2545cc9
Glitch (coffeetime-new) <none>
2019-03-12 00:41:39 +0000

    🚏👡 Checkpoint ./coffee.test.js:491323/1770

--- coffee.test.js
+++ coffee.test.js
@@ -37,3 +37,19 @@ test('coffee time should not pair anyone with themseleves', () => {
     expect(uniques).toEqual(pair);
   });
 });
+
+
+test('coffee time should not pair people it has already paired unless it has to', () => {
+  const users = [1, 2, 3, 4, 5];
+  const past = 		[
+				["3-8", "2-7", "4-11", "1-5", "0-9", "6-8", "3-6"],
+				["3-5", "0-7", "6-11", "1-2", "4-8", "7-9", "0-9"],
+				["3-4", "2-5", "8-11", "1-9", "0-6", "7-11", "7-8"],
+				["0-4", "3-9", "2-6", "5-8", "1-11", "5-7", "7-8"],
+				["1-6", "2-11", "5-9", "0-8", "3-7", "4-6", "1-4"]
+			]
+		
+  const coffeepairsFirst = coffee.pairUsers(users, '');
+
+  
+});
