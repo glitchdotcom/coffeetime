cb7aec54df7154f408f911201d97f8f54a364ba8
Glitch (coffeetime-new) <none>
2019-03-12 00:08:39 +0000

    🛥💍 Checkpoint ./coffee.test.js:491323/1925 ./coffee.json:491323/1176

--- coffee.test.js
+++ coffee.test.js
@@ -1,24 +1,29 @@
 const fs = require('fs');
 const coffee = require('./coffee');
 
-const jsonData = fs.readFileSync('coffee.json').toString('utf8')
+const jsonData = fs.readFileSync('coffee.json').toString('utf8');
 const data = JSON.parse(jsonData);
 
+function 
 
 
 test('coffee time should pair everyone', () => {
-  const users = [1, 2, 3, 4]
+  const users = [1, 2, 3, 4];
   const coffeepairs = coffee.pairUsers(users, '');
   console.log(coffeepairs);
-    console.log(coffeepairs.pairs);
+  console.log(coffeepairs.pairs);
 
-const pairs = coffeepairs.pairs;
-var flattened = pairs.reduce(
-  function(accumulator, currentValue) {
+  const pairs = coffeepairs.pairs;
+  var flattened = pairs.reduce(function(accumulator, currentValue) {
     return accumulator.concat(currentValue);
-  },
-  []
-);
-  expect(flattened).toEqual(expect.arrayContaining(users))
+  }, []);
+  expect(flattened).toEqual(expect.arrayContaining(users));
+});
+
+
+test('coffee time should not pair anyone with themseleves', () => {
+  const users = [1, 2, 3, 4];
+  const coffeepairs = coffee.pairUsers(users, '');
+
 
 });
