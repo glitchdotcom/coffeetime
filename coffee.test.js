a7d922b344768b310d1aab3b421f7c6fb9434f83
Glitch (coffeetime-new) <none>
2019-03-12 00:30:38 +0000

    🐌🌼 Checkpoint ./coffee.test.js:491323/3858

--- coffee.test.js
+++ coffee.test.js
@@ -4,11 +4,10 @@ const coffee = require('./coffee');
 const jsonData = fs.readFileSync('coffee.json').toString('utf8');
 const data = JSON.parse(jsonData);
 
-function createFakeUsers(){
-  const randomNumber = Math.floor(Math.random() * (54 - 4) ) + 4;
-  return Array.from({length: randomNumber}, (v, i) => i + 1);
+function createFakeUsers() {
+  const randomNumber = Math.floor(Math.random() * (54 - 4)) + 4;
+  return Array.from({ length: randomNumber }, (v, i) => i + 1);
 }
-
 
 test('coffee time should pair everyone', () => {
   const users = createFakeUsers();
@@ -20,15 +19,21 @@ test('coffee time should pair everyone', () => {
   expect(flattened).toEqual(expect.arrayContaining(users));
 });
 
-
-test('coffee time should not pair anyone with themseleves', () => {
-  const users = createFakeUsers;
-    const coffeepairs = coffee.pairUsers(users, '');
+test('coffee time should not create a pair with only one person', () => {
+  const users = createFakeUsers();
+  const coffeepairs = coffee.pairUsers(users, '');
   const pairs = coffeepairs.pairs;
-  pairs.forEach(function(pair) {
-    
+  expect(pairs.every(x => x.length >= 2)).toBe(true);
 });
 
-  
-
+test('coffee time should not pair anyone with themseleves', () => {
+  const users = createFakeUsers();
+  const coffeepairs = coffee.pairUsers(users, '');
+  const pairs = coffeepairs.pairs;
+  pairs.forEach(function(pair) {
+    const uniques = pair.filter(function(value, index, self) {
+      return self.indexOf(value) === index;
+    });
+    expect(uniques).toEqual(pair);
+  });
 });
