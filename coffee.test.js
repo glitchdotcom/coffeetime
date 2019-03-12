482da899948522ab6c0542e65dea18b3e90704d2
Glitch (coffeetime-new) <none>
2019-03-12 01:14:39 +0000

    ⛈🙌 Checkpoint ./coffee.test.js:491323/6250

--- coffee.test.js
+++ coffee.test.js
@@ -13,6 +13,8 @@ test('coffee time should pair everyone', () => {
   const users = createFakeUsers();
   const coffeepairs = coffee.pairUsers(users, '');
   const pairs = coffeepairs.pairs;
+  console.log(users);
+  console.log(coffeepairs);
   var flattened = pairs.reduce(function(accumulator, currentValue) {
     return accumulator.concat(currentValue);
   }, []);
@@ -23,7 +25,7 @@ test('coffee time should not create a pair with only one person', () => {
   const users = createFakeUsers();
   const coffeepairs = coffee.pairUsers(users, '');
   const pairs = coffeepairs.pairs;
-  expect(pairs.every(x => x.length >= 2)).toBe(true);
+  expect(pairs.every((x) => x.length >= 2)).toBe(true);
 });
 
 test('coffee time should not pair anyone with themseleves', () => {
@@ -38,18 +40,31 @@ test('coffee time should not pair anyone with themseleves', () => {
   });
 });
 
+//this is a test function to see if the data from the stringy past pairs works the same
+function numberArray(stringArray) {
+  let flattened = stringArray.reduce(function(accumulator, currentValue) {
+    return accumulator.concat(currentValue);
+  }, []);
+
+  return flattened.map((x) => {
+    return x.split('-').map(Number);
+  });
+}
 
 test('coffee time should not pair people it has already paired unless it has to', () => {
-  const users = [1, 2, 3, 4, 5];
-  const past = 		[
-				["3-8", "2-7", "4-11", "1-5", "0-9", "6-8", "3-6"],
-				["3-5", "0-7", "6-11", "1-2", "4-8", "7-9", "0-9"],
-				["3-4", "2-5", "8-11", "1-9", "0-6", "7-11", "7-8"],
-				["0-4", "3-9", "2-6", "5-8", "1-11", "5-7", "7-8"],
-				["1-6", "2-11", "5-9", "0-8", "3-7", "4-6", "1-4"]
-			]
-		
-  const coffeepairsFirst = coffee.pairUsers(users, '');
-
+  const users = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ];
+  const past = [ '6-14', '1-12', '5-10', '2-4', '8-11', '3-13', '7-9' ];
   
+  //let's turn into an array of arrays
+  const converted = numberArray(converted);
+  // let's pair again
+  const coffeepairs = coffee.pairUsers(users, past);
+    const pairs = coffeepairs.pairs;
+  pairs.forEach(function(pair) {
+    const uniques = pair.filter(function(value, index, self) {
+      return self.indexOf(value) === index;
+    });
+    expect(uniques).toEqual(pair);
+  });
+
 });
