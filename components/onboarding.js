f9fac59c89cb32671820548bf73c3a53ba682737
Glitch (coffeetime-new) <none>
2019-03-26 19:24:17 +0000

    ⛲️🌟 Checkpoint ./components/onboarding.js:213918/6

--- components/onboarding.js
+++ components/onboarding.js
@@ -6,10 +6,10 @@ module.exports = function(controller) {
         debug('Starting an onboarding experience!');
         bot.startPrivateConversation({user: bot.config.createdBy},function(err,convo) {
             if (err) {
-            console.log(err);
+              console.log(err);
             } else {
-            convo.say('I am a bot that has just joined your team');
-            convo.say('You must now /invite me to a channel so that I can be of use!');
+              convo.say('I am a bot that has just joined your team');
+              convo.say('You must now /invite me to a channel so that I can be of use!');
             }
         });
     });
