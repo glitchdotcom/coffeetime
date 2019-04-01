55b7017703afe2dfbc26254b8da0a8267b4c8fcc
Glitch (stupendous-traffic) <none>
2019-04-01 19:37:17 +0000

    🎢✈️ Checkpoint ./skills/block_actions/help.js:213918/737

--- skills/block_actions/help.js
+++ skills/block_actions/help.js
@@ -21,24 +21,9 @@ module.exports = function(controller) {
   });
   controller.hears(['^help'], 'direct_message,direct_mention', function(bot, message) {
     const blocks = [
-      ];
+      blocksBuilder.section("Hi there! Welcome to *CoffeeTime*!")
+    ];
     bot.reply(message, { blocks });
-    bot.createConversation(message, function(err, convo) {
-      const data = storage.loadData();
-
-      if (user.checkForUser(message.user, data)) {
-        convo.say('You are all set! Use `unsubscribe` to stop pairing.');
-        const managerId = user.getManager(message.user);
-        if (managerId) {
-          convo.say(`<@${managerId}> is your manager, you won't pair with them`);
-        } else {
-          convo.say(`You don't have a manager set.  Set your manager with 'manager @<Your_Manager>'`);
-        }
-      } else {
-        convo.say('You are not subscribed yet. Use `subscribe` to change that.');
-      }
-      convo.activate();
-    });
   });
   
   controller.hears(['^oldhelp'], 'direct_message,direct_mention', function(bot, message) {
